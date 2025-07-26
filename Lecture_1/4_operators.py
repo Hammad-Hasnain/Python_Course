d474dfbba541de43e0bf6e73361ca2e5a210998b
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-07-26 16:58:11 +0500

    feat(Lec_1): assignement, arithmetic, relatinal, logical and arithmetic assignment operators

---
+++ Lecture_1/4_operators.py
@@ -0,0 +1,66 @@
+# Assignment Operators
+a = 20 
+b = 10 
+
+
+
+# Arithmetic Operators
+"""
+print(a + b) # sum
+print(a - b) # subtract 
+print(a * b) # product
+print(a / b) # divide
+print(a % b) # remainder
+"""
+
+
+
+# Relational / Comparision Operators
+"""
+print(a == b ) # equal to operator
+print(a != b ) # not equal to operator
+print(a > b )  # greater than operator
+print(a < b )  # less than operator
+print(a >= b ) # greater than equal to operator
+print(a <= b ) # less than equal to operator
+"""
+
+
+
+# Arithmetic Assignement Operators
+c = 10
+d = 10
+e = 10
+f = 10
+g = 10   
+h = 10   
+
+c+=2
+d -= 2
+e *= 2
+f /= 2
+g %= 2
+h **= 2
+"""
+print(c)    # c = c + 2
+print(d)    # d = d - 2
+print(e)    # e = e * 2
+print(f)    # f = f / 2
+print(g)    # g = g % 2
+print(h)    # h = h ** 2
+"""
+
+
+# Logical Operators
+
+print(not True)  # not operator 
+
+print(True and True)  # and operator
+print(False and True)
+print(True and False)
+print(False and False)
+
+print(True or True)  #  or operator
+print(False or True)
+print(True or False)
+print(False or False)
