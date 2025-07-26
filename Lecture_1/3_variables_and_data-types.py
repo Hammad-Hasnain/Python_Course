ff72b3fde705cc92cd9c834bc0bf40f46ffc05f4
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-07-26 15:15:30 +0500

    feat(Lec_1): data types and variables

---
+++ Lecture_1/3_variables_and_data-types.py
@@ -0,0 +1,38 @@
+# Note : Remove multi line comments (""" """) to see the result
+
+# Approach 1: Directly printing data
+
+"""
+print("Hello World")  
+
+print(22)
+
+print(99.99)
+
+print(True)
+print(False)
+
+print(None) 
+"""
+
+# Approach 2: Printing data with the help of variables
+string_var = "Hello World"
+int_var = 22
+float_var = 99.99
+bool_var = True
+none_var = None
+
+"""
+print(string_var)
+print(int_var)
+print(float_var)
+print(bool_var)
+print(none_var)
+"""
+
+# Finding Data Types
+print(type(string_var))
+print(type(int_var))
+print(type(float_var))
+print(type(bool_var))
+print(type(none_var))
