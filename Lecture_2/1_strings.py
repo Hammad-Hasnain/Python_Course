8312b065e98dd1ab67f719d0455ad86c77192f7b
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-07-28 12:06:06 +0500

    feat(Lec_2): strings

---
+++ Lecture_2/1_strings.py
@@ -0,0 +1,10 @@
+# Four ways to define string
+str1 = 'She said, "Hello!"' 
+str2 =  "It's a great day." 
+str3 = """She said, "It's a beautiful day!" and smiled."""       # Triple Double Quotes contains multiple lines of strings.
+str4 = '''It's amazing how she said, "Don't worry about it."'''  # Triple Single Quotes also contains multiple lines of strings.
+
+print(str1)  # Output: She said, "Hello!"
+print(str2)  # Output: It's a great day.
+print(str3)  # Output: She said, "It's a beautiful day!" and smiled.
+print(str4)  # Output: It's amazing how she said, "Don't worry about it."
