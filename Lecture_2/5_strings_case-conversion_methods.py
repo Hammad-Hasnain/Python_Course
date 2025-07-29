201e539e7e74eb00e42d443dcabb02e2e82c2870
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-07-29 12:10:59 +0500

    feat(Lec_2): string case conversion methods

---
+++ Lecture_2/5_strings_case-conversion_methods.py
@@ -0,0 +1,35 @@
+# ------------------------------ String Case Conversion Methods ------------------------------
+
+
+# 1. lower(): Converts all characters to lowercase.
+str1 = "HELLO WORLD" 
+updatedStr1= str1.lower() 
+print(updatedStr1) # Output: hello world
+
+
+
+# 2. upper(): Converts all characters to uppercase.
+str2 = "hello world" 
+updatedStr2= str2.upper() 
+print(updatedStr2) # Output: HELLO WORLD
+
+
+
+# 3. capitalize(): Capitalizes the first character, rest lowercase.
+str3 = "hello World" 
+updatedStr3= str3.capitalize() 
+print(updatedStr3) # Output: Hello world
+
+
+
+# 4. title():  Capitalizes the first character of each word.
+str4 = "hello world" 
+updatedStr4= str4.title() 
+print(updatedStr4) # Output: Hello World
+
+
+
+# 5. swapcase(): Swaps uppercase to lowercase and vice versa.
+str5 = "HELLO world"
+updatedStr5= str5.swapcase() 
+print(updatedStr5) # Output: hello WORLD
